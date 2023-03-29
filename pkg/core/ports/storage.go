5cea62ec37895437f55d41c88a74136875792f88
John Hughes <john.hughes@musictribe.com>
2023-03-29 17:21:59 +0100

    feat: :sparkles: Add generated mocks
    Generate mocks via Makefile
    
    mocks remain untracked

--- pkg/core/ports/storage.go
+++ pkg/core/ports/storage.go
@@ -5,6 +5,7 @@ import (
 	"os"
 )
 
+//go:generate mockgen -destination=./../../actors/repositories/storage/mockstorage/mock_store.go -package=mockstorage -source=storage.go Storage
 type Storage interface {
 	Add(storagePath string, image io.Reader) error
 	GetByStoragePath(storagePath string) (*os.File, error)
